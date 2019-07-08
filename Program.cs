743d66a14e2b0a36203a40dda1a716357b2cd516
charles-desouza <charles5388@gmail.com>
2019-07-08 15:06:33 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreRT_Sample_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
